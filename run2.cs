4c1d26daf296d3af4e3dcfb0c4d874a7495fedf4
Takunori Min <37891479+NoriMin@users.noreply.github.com>
2020-10-09 17:05:44 +0900

    Add files via upload

---
+++ run2.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading.Tasks;
+
+public static void Run(string mySbMsg, Int 32 deliveryCount, DateTime enqueuedTimeUtc, string messageId, TraceWriter log)
+{
+    log.Info($"C# {mySbMsg}");
+    log.Info($"{enqueuedTimeUtc}");
+    log.Info($"{deliveryCount}");
+    log.Info($"{messageId}");
+}
