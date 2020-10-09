a2408f09d907799f791f923b31f245aa600d2ed4
Takunori Min <37891479+NoriMin@users.noreply.github.com>
2020-10-09 17:06:56 +0900

    Update run2.cs

--- run2.cs
+++ run2.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading.Tasks;
 
-public static void Run(string mySbMsg, Int 32 deliveryCount, DateTime enqueuedTimeUtc, string messageId, TraceWriter log)
+public static void Run(string mySbMsg, Int32 deliveryCount, DateTime enqueuedTimeUtc, string messageId, TraceWriter log)
 {
     log.Info($"C# {mySbMsg}");
     log.Info($"{enqueuedTimeUtc}");
